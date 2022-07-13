3e1464327f670c24b949e84781febd09c6530611
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-07-13 02:42:15 -0300

    Mapeamento Objeto relacional - Lombok

--- src/main/java/br/com/fiap/conexao/entity/Pessoa.java
+++ src/main/java/br/com/fiap/conexao/entity/Pessoa.java
@@ -1,51 +1,30 @@
 package br.com.fiap.conexao.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PESSOA")
+	@SequenceGenerator(name = "SEQ_PESSOA", sequenceName = "SEQ_PESSOA", allocationSize = 1, initialValue = 1)
 	Integer id;
 
 	String nome;
 
 	String sobrenome;
-
-	public Pessoa() {
-		super();
-	}
-
-	public Pessoa(Integer id, String nome, String sobrenome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.sobrenome = sobrenome;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-
-	@Override
-	public String toString() {
-		return "Pessoa [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + "]";
-	}
 
 }
